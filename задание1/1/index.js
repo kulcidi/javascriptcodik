3fb66e193ddb96f58e67d459c0ca9390838867e5
kulcidi <kullciid252@gmail.com>
2024-02-01 14:06:23 +0900

    task 1 2

---
+++ задание1/1/index.js
@@ -0,0 +1,15 @@
+// comment 
+/* Автор:
+Иля*/
+
+const myName = 'Ilya';
+const mySurname = 'Pankratova';
+let myFavoriteDrink = 'Cola';
+let myFavoriteAnimal = 'Dog';
+let myFavoriteProgrammingLanguage = 'JavaScript';
+
+console.log(myName)
+console.log(mySurname)
+console.log(myFavoriteDrink)
+console.log(myFavoriteAnimal)
+console.log(myFavoriteProgrammingLanguage)
---
+++ задание1/2/car.js
@@ -0,0 +1,25 @@
+const carName = 'Subebe';
+let maxCarSpeed = '450km/h';
+let carOwner = '';
+
+alert(carName);
+alert(maxCarSpeed);
+alert(carOwner);
+
+console.log( carName);
+console.log (maxCarSpeed);
+console.log (carOwner);
+
+maxCarSpeed = '425km/h';
+console.log('У машины новая скорость: ', maxCarSpeed);
+maxCarSpeed = '400km/h';
+console.log('У машины новая скорость: ', maxCarSpeed);
+maxCarSpeed = '375km/h';
+console.log('У машины новая скорость: ', maxCarSpeed);
+
+carOwner = 'Юля';
+console.log('У машины появился владелец: ', carOwner);
+carOwner = 'Ваня';
+console.log('У машины появился новый владелец: ', carOwner);
+carOwner = 'Илья';
+console.log('У машины появился новый владелец: ', carOwner);
