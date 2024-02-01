b6c13ed7d7733f42580fc514fa0467c615406043
kulcidi <kullciid252@gmail.com>
2024-02-02 00:43:20 +0900

    complete 3.1

---
+++ задание3/1/undex.js
@@ -0,0 +1,19 @@
+const number = 1;
+console.log(Number(number), Boolean(number), String(number))
+const stroke = 'text';
+console.log(Number(stroke), Boolean(stroke), String(stroke))
+const undefiined = undefined;
+console.log(Number(undefiined), Boolean(undefiined), String(undefiined))
+const nulll = null;
+console.log(Number(nulll), Boolean(nulll), String(nulll))
+const bigstring = 445651511245481n;
+console.log(Number(bigstring), Boolean(bigstring), String(bigstring))
+const objectt = {
+    name: 'илья',
+    age: 18,
+};
+console.log(Number(objectt), Boolean(objectt), String(objectt))
+const symboll = Symbol('l');
+console.log(Boolean(symboll), String(symboll))
+const trueorfalse = true;
+console.log(Number(trueorfalse), Boolean(trueorfalse), String(trueorfalse))
