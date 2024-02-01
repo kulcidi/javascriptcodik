d66ecbc870ffe98f8fa8e23fd7395dce261f38cb
kulcidi <kullciid252@gmail.com>
2024-02-02 08:36:30 +0900

    complete 3.3

---
+++ задание3/3/index.js
@@ -0,0 +1,31 @@
+console.log(Number(console.log));
+console.log(String(console.log));
+console.log(Boolean(console.log));
+
+console.log(Number({ name : 'Maxim'}));
+console.log(String({ name : 'Maxim'}));
+console.log(Boolean({ name : 'Maxim'}));
+
+// console.log(Number(Symbol('key')));
+console.log(String(Symbol('key')));
+console.log(Boolean(Symbol('key')));
+
+console.log(Number(Number));
+console.log(String(Number));
+console.log(Boolean(Number));
+
+console.log(Number('"'));
+console.log(String('"'));
+console.log(Boolean('"'));
+
+console.log(Number(0));
+console.log(String(0));
+console.log(Boolean(0));
+
+console.log(Number(-10));
+console.log(String(-10));
+console.log(Boolean(-10));
+
+console.log(Number('-105'));
+console.log(String('-105'));
+console.log(Boolean('-105'));
