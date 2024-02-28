dffbfb63d2e3da5cefa67109bcd05e23bf8543d3
kulcidi <kullciid252@gmail.com>
2024-02-28 09:40:43 +0900

    completye 4.2

---
+++ задание4/1/index.js
@@ -0,0 +1,7 @@
+const myName = 'bebebe';
+const programmingLanguage = 'js';
+const courseCreatorName = 'tfuyg';
+const reasonText = 'bkvbeidb';
+const numberOfMonth = '456';
+
+console.log(`Всем привет! Меня зовут ${myName}. Сейчас я изучаю язык программирования ${programmingLanguage} на курсе по ${programmingLanguage} у ${courseCreatorName}. Я хочу стать веб-разработчиком, потому что ${reasonText}. До этого я изучала ${programmingLanguage} ${numberOfMonth} месяцев. Я уверена, что пройду данный курс до конца!`);
