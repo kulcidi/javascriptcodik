2c39131620c1eb4128477ac1a08a517bfa23ee5f
kulcidi <kullciid252@gmail.com>
2024-02-29 08:43:08 +0900

    complete 4 task

--- задание4/34/index.js
+++ задание4/34/index.js
@@ -2,4 +2,9 @@ let userAnswer = prompt('Как вас зовут?');
 userAnswer = userAnswer.toUpperCase();
 userAnswer = userAnswer.replace(/\s+/g, '');
 
+let userAge = prompt('Сколько вам лет?');
+userAge = Number;
+userAge = userAge.replace(/\s+/g, '');
+
 alert(`Ваше имя: ${userAnswer}`);
+alert(`Ваше возраст: ${userAge}`);
